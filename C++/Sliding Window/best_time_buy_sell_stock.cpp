e72c4fec5dd37e681f8a951def4993d38d1cf3a8
Arvindh Vijayakumar <141946284+gangue-anna@users.noreply.github.com>
2025-08-07 12:58:15 +0530

    Updated best_time_buy_sell_stock.cpp

--- C++/Sliding Window/best_time_buy_sell_stock.cpp
+++ C++/Sliding Window/best_time_buy_sell_stock.cpp
@@ -76,3 +76,17 @@ public:
  * - Uses O(1) space with only variables
  * - Handles edge cases properly
  */
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        int min_price = INT_MAX;
+        for(int i = 0; i < prices.size(); i++){
+            int curr_profit = prices[i] - min_price;
+            max_profit = max(max_profit, curr_profit);
+            min_price = min(min_price, prices[i]);
+        }
+        return max_profit;
+    }
+};
