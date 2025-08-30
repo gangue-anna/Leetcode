fe6eb164423e4e10a23006ee96aaf13d87b0dd65
Arvindh Vijayakumar <141946284+gangue-anna@users.noreply.github.com>
2025-08-30 16:38:51 +0530

    Updated house_robber.py

--- python/1-D Dynamic Programming/house_robber.py
+++ python/1-D Dynamic Programming/house_robber.py
@@ -19,6 +19,24 @@ class Solution:
 # Time Complexity: O(n), where n is the number of houses (nums)
 # Space Complexity: O(1), since only two variables are used regardless of input size
 
+class Solution2:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[1], nums[0])
+        n = len(nums)
+
+        for i in range(2, n):
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+        
+        return dp[n - 1]
+
 # -------------------
 # Test Cases
 # -------------------
