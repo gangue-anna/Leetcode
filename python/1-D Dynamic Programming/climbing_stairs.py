59f8f3707bfb865b3d1d3ac96a44b4a1aef90cd8
Arvindh Vijayakumar <141946284+gangue-anna@users.noreply.github.com>
2025-08-30 12:02:40 +0530

    Updated climbing_stairs.py

--- python/1-D Dynamic Programming/climbing_stairs.py
+++ python/1-D Dynamic Programming/climbing_stairs.py
@@ -28,6 +28,17 @@ class Solution:
             q = temp + q
         return q
 
+class Solution2:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[n] = 1
+        dp[n - 1] = 1
+
+        for i in range(n-2, -1, -1):
+            dp[i] = dp[i + 1] + dp[i + 2]
+        
+        return dp[0]
+
 def test_climb_stairs():
     solution = Solution()
     # Test case 1: n = 2
